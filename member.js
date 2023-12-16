8e7e0c831e6ee533ebc1e4c13ef0ae877dd3e004
sungfuchiu <sungfuchiu@gmail.com>
2023-12-16 17:31:45 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,31 @@
+function skilsMember() {
+    var member = document.getElementById("member");
+    var memberName = member.options[member.selectedIndex].text;
+    var memberValue = member.options[member.selectedIndex].value;
+    var memberSkills = document.getElementById("memberSkills");
+    var memberSkillsName = memberSkills.options[memberSkills.selectedIndex].text;
+    var memberSkillsValue = memberSkills.options[memberSkills.selectedIndex].value;
+    var memberSkillsList = document.getElementById("memberSkillsList");
+    var memberSkillsListName = memberSkillsList.options[memberSkillsList.selectedIndex].text;
+    var memberSkillsListValue = memberSkillsList.options[memberSkillsList.selectedIndex].value;
+    var memberSkillsListValueName = memberSkillsList.options[memberSkillsList.selectedIndex].value;
+    var memberSkillsListValueNameText = memberSkillsList.options[memberSkillsList.selectedIndex].text;
+    var memberSkillsListValueNameTextLength = memberSkillsList.options[memberSkillsList.selectedIndex].text.length;
+    var memberSkillsListValueNameTextLength = memberSkillsList.options[memberSkillsList.selectedIndex].text.length;
+    var memberSkillsListValueNameTextLength = memberSkillsList.options[memberSkillsList.selectedIndex].text.length;
+    var memberSkillsListValueNameTextLength = memberSkillsList.options[memberSkillsList.selectedIndex].text.length;
+    var memberSkillsListValueNameTextLength = memberSkillsList.options[memberSkillsList.selectedIndex].text.length;
+    var memberSkillsListValueNameTextLength = memberSkillsList.options[memberSkillsList.selectedIndex].text.length;
+    var memberSkillsListValueNameTextLength = memberSkillsList.options[memberSkillsList.selectedIndex].text.length;
+    var memberSkillsListValueNameTextLength = memberSkillsList.options[memberSkillsList.selectedIndex].text.length;
+    var memberSkillsListValueNameTextLength = memberSkillsList.options[memberSkillsList.selectedIndex].text.length;
+    var memberSkillsListValueNameTextLength = memberSkillsList.options[memberSkillsList.selectedIndex].text.length;
+
+    var memberSkillsListValueNameTextLength = memberSkillsList.options[memberSkillsList.selectedIndex].text.length;
+    var memberSkillsListValueNameTextLength = memberSkillsList.options[memberSkillsList.selectedIndex].text.length;
+
+    var memberSkillsListValueNameTextLength = memberSkillsList.options[memberSkillsList.selectedIndex].text.length;
+    var memberSkillsListValueNameTextLength = memberSkillsList.options[memberSkillsList.selectedIndex].text.length;
+
+    var memberSkillsList = document.getElementById("memberSkillsList");
+}
