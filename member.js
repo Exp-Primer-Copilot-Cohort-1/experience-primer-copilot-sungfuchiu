2a8b8022aab54a4b9171525c712dff9f0b4ec9b5
sungfuchiu <sungfuchiu@gmail.com>
2023-12-16 17:40:13 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,31 +1,9 @@
-function skilsMember() {
+function skillsMember() {
     var member = document.getElementById("member");
-    var memberName = member.options[member.selectedIndex].text;
     var memberValue = member.options[member.selectedIndex].value;
-    var memberSkills = document.getElementById("memberSkills");
-    var memberSkillsName = memberSkills.options[memberSkills.selectedIndex].text;
-    var memberSkillsValue = memberSkills.options[memberSkills.selectedIndex].value;
-    var memberSkillsList = document.getElementById("memberSkillsList");
-    var memberSkillsListName = memberSkillsList.options[memberSkillsList.selectedIndex].text;
-    var memberSkillsListValue = memberSkillsList.options[memberSkillsList.selectedIndex].value;
-    var memberSkillsListValueName = memberSkillsList.options[memberSkillsList.selectedIndex].value;
-    var memberSkillsListValueNameText = memberSkillsList.options[memberSkillsList.selectedIndex].text;
-    var memberSkillsListValueNameTextLength = memberSkillsList.options[memberSkillsList.selectedIndex].text.length;
-    var memberSkillsListValueNameTextLength = memberSkillsList.options[memberSkillsList.selectedIndex].text.length;
-    var memberSkillsListValueNameTextLength = memberSkillsList.options[memberSkillsList.selectedIndex].text.length;
-    var memberSkillsListValueNameTextLength = memberSkillsList.options[memberSkillsList.selectedIndex].text.length;
-    var memberSkillsListValueNameTextLength = memberSkillsList.options[memberSkillsList.selectedIndex].text.length;
-    var memberSkillsListValueNameTextLength = memberSkillsList.options[memberSkillsList.selectedIndex].text.length;
-    var memberSkillsListValueNameTextLength = memberSkillsList.options[memberSkillsList.selectedIndex].text.length;
-    var memberSkillsListValueNameTextLength = memberSkillsList.options[memberSkillsList.selectedIndex].text.length;
-    var memberSkillsListValueNameTextLength = memberSkillsList.options[memberSkillsList.selectedIndex].text.length;
-    var memberSkillsListValueNameTextLength = memberSkillsList.options[memberSkillsList.selectedIndex].text.length;
-
-    var memberSkillsListValueNameTextLength = memberSkillsList.options[memberSkillsList.selectedIndex].text.length;
-    var memberSkillsListValueNameTextLength = memberSkillsList.options[memberSkillsList.selectedIndex].text.length;
-
-    var memberSkillsListValueNameTextLength = memberSkillsList.options[memberSkillsList.selectedIndex].text.length;
-    var memberSkillsListValueNameTextLength = memberSkillsList.options[memberSkillsList.selectedIndex].text.length;
-
-    var memberSkillsList = document.getElementById("memberSkillsList");
+    if (memberValue == "yes") {
+        document.getElementById("skills").style.display = "block";
+    } else {
+        document.getElementById("skills").style.display = "none";
+    }
 }
